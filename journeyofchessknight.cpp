02277bb7b2b116a9e927c760a069b65154350719
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-21 14:14:42 +0300

    Вручную инициализируем массивы сдвигов значений ходов для шахматного коня.

--- journeyofchessknight.cpp
+++ journeyofchessknight.cpp
@@ -3,5 +3,7 @@
 JourneyOfChessKnight::JourneyOfChessKnight(QObject *parent)
     : QObject{parent}
 {
-
+    // инициализация массивов ходов шахматного коня
+    movesX = new short[movesSize] {2, 1, -1, -2, -2, -1, 1, 2};
+    movesY = new short[movesSize] {1, 2, 2, 1, -1, -2, -2, -1};
 }
