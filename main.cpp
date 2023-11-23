199ef534e89ef6d5ab99f2dc74a6f258c9eeedb8
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-23 12:30:45 +0300

    Одна из многих тестовых реализаций основного метода..

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
 
+#include "chessboard.h"
+#include "journeyofchessknight.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Chessboard *chessboard = new Chessboard(5);
+    chessboard->clear();
+    JourneyOfChessKnight *knightJournay = new JourneyOfChessKnight();
+
+    qInfo() << "Start application";
+    chessboard->print();
+    knightJournay->startTour(chessboard, 2, 2);
+    qInfo() << "After journay";
+    chessboard->print();
 
     return a.exec();
 }
