8786f7b9abab7b240709a32c63be0e01eb30bdfa
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-23 10:09:07 +0300

    Глобальная переработка методов в связи с новым подходом.
    Подход работает лучше предыдущего, но все-равно пока еще имеет недостатки в ответе.

--- journeyofchessknight.cpp
+++ journeyofchessknight.cpp
@@ -10,68 +10,81 @@ JourneyOfChessKnight::JourneyOfChessKnight(QObject *parent)
 
 bool JourneyOfChessKnight::canBeDone(Chessboard *chessboard,
                                      short currentX,
-                                     short currentY)
+                                     short currentY,
+                                     short i)
 {
     // сначала мы сразу заполняем ячейку номером сделанного
     // хода, и в случае неудачи, можем даже "откатить операцию"
-    chessboard->getCells()[nextX][nextY] = i;
-    tryNextMove(chessboard, currentX, currentY);
+    chessboard->getCells()[currentX][currentY] = i;
+    tryNextMove(chessboard, currentX, currentY, i);
     if (!isDone) {
-        chessboard->getCells()[nextX][nextY] = 0;
-    }
+        chessboard->getCells()[currentX][currentY] = 0;
+    }    
     return isDone;
 }
 
 void JourneyOfChessKnight::next(Chessboard *chessboard,
-                                short currentX,
-                                short currentY)
+                                short *nextX,
+                                short *nextY,
+                                short *k,
+                                bool *eos)
 {
     // пока координаты следующего хода находятся в пределах
     // доски и сами варианты ходов не перебраны - все впорядке
     do {
-        k++;
-        if (k < 8) {
-            nextX = currentX + movesX[k];
-            nextY = currentY + movesY[k];
+        (*k)++;
+        if (*k < 8) {
+            *nextX = beginX + movesX[*k];
+            *nextY = beginY + movesY[*k];
         }
         // в случае успешности выбора координат - останавливаем цикл
-        if ((0 <= nextX) &&
-            (nextX < chessboard->getSize()) &&
-            (0 <= nextY) &&
-            (nextY < chessboard->getSize()) &&
-            (chessboard->getCells()[nextX][nextY]) == 0) {
+        if ((0 <= *nextX) &&
+            (*nextX < chessboard->getSize()) &&
+            (0 <= *nextY) &&
+            (*nextY < chessboard->getSize()) &&
+            (chessboard->getCells()[*nextX][*nextY]) == 0) {
             break;
         }
-    } while (k != 8);
-    eos = (k == 8);
+    } while (*k != 8);
+    *eos = (*k == 8);
 }
 
 
 void JourneyOfChessKnight::first(Chessboard *chessboard,
-                                 short currentX,
-                                 short currentY)
+                                 short *nextX,
+                                 short *nextY,
+                                 short *k,
+                                 bool *eos)
 {
-    eos = false;
-    k = -1; // таким образом косвенно выбираем ход
-    next(chessboard, currentX, currentY);
+    *eos = false;
+    *k = -1; // таким образом косвенно выбираем ход
+    next(chessboard, nextX, nextY, k, eos);
 }
 
 void JourneyOfChessKnight::tryNextMove(Chessboard *chessboard,
                                        short currentX,
-                                       short currentY)
+                                       short currentY,
+                                       short i)
 {
+    // подготовка
+    beginX = currentX;
+    beginY = currentY;
+    short *nextX = new short; // координата следующего потенциального хода
+    short *nextY = new short; // координата следующего потенциального хода
+    short *k = new short; // вспомогательный индекс хода
+    bool *eos = new bool; // условие, что ходов больше нет
+
     // пытаемся сделать ход пока есть место на доске
     if (i < chessboard->getSize()*
             chessboard->getSize()) {
-        first(chessboard, currentX, currentY); // в приоритете всегда первый вариант хода коня
-        i++;
+        first(chessboard, nextX, nextY, k, eos); // в приоритете всегда первый вариант хода коня
         // пытаемся найти доступный следующий ход
-        while (!eos && !canBeDone(chessboard, currentX, currentY)) {
-            next(chessboard, currentX, currentY);
+        while (!(*eos) && !canBeDone(chessboard, *nextX, *nextY, i+1)) {
+            next(chessboard, nextX, nextY, k, eos);
         }
         isDone = !eos;
     } else {
-        isDone = true;
+        isDone = true;        
     }
 }
 
@@ -86,8 +99,7 @@ void JourneyOfChessKnight::startTour(Chessboard *chessboard,
 {
     clear(chessboard);
     // cразу заполняем значение стартовой ячейки
-    // пытаемся сделать следующий ход
-    i = 1;
-    chessboard->getCells()[startX][startY] = i;
-    tryNextMove(chessboard, startX, startY);
+    // пытаемся сделать следующий ПЕРВЫЙ ход
+    chessboard->getCells()[startX][startY] = 1;
+    tryNextMove(chessboard, startX, startY, 1);
 }
