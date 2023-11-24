a0868fcfbb20e23f49978a50dc591f2b4db95fb3
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-24 13:37:53 +0300

    Добавлена реализация метода next для поиска нужной позиции ферзя

--- eightqueensproblem.cpp
+++ eightqueensproblem.cpp
@@ -5,3 +5,18 @@ EightQueensProblem::EightQueensProblem(QObject *parent)
 {
 
 }
+
+void EightQueensProblem::next(bool *endOfOptions,
+                              short *j,
+                              short i)
+{
+    // поиск безопасной позиции
+    do {
+        (*j)++;
+        // если нашли ее, то останавливаемся
+        if (a[*j] && b[i+(*j)] && c[i-(*j) + correctChessboardSize - 1]) {
+            break;
+        }
+    } while (*j != correctChessboardSize);
+    *endOfOptions = (*j == correctChessboardSize);
+}
