aaaa5914e81cd696be272091f4efac7158dbc11c
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-27 15:57:46 +0300

    Тестирование классов для новой задачи в главном методе.

--- main.cpp
+++ main.cpp
@@ -3,21 +3,25 @@
 #include "journeyofchessknight.h"
 #include "eightqueensproblem.h"
 
+#include "Stable marriages task/man.h"
+#include "Stable marriages task/women.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Chessboard *chessboard = new Chessboard(8);
-    chessboard->clear();    
+    //Chessboard *chessboard = new Chessboard(8);
+    //chessboard->clear();
 
-    qInfo() << "Start application";
-    chessboard->print();
+    //qInfo() << "Start application";
+    //chessboard->print();
 
     //JourneyOfChessKnight *knightJournay = new JourneyOfChessKnight();
     //knightJournay->startTour(chessboard, 2, 2);
     //qInfo() << "After journay";
     //chessboard->print();
 
+    /*
     EightQueensProblem *queensProblem = new EightQueensProblem();
     queensProblem->setChessboard(chessboard);
     // если нужно найти первое доступное решение задачи
@@ -31,6 +35,15 @@ int main(int argc, char *argv[])
     // чтобы найти все доступные решения
     short solutionsCount = queensProblem->allSolutions();
     qInfo() << "Solutions founded: " << solutionsCount;
+    */
+
+    QList<Women *> womens;
+    womens.append(new Women);
+    womens.append(new Women);
+    womens.append(new Women);
+
+    Man manSome;
+    manSome.randomlyFillRates(womens);
 
     return a.exec();
 }
