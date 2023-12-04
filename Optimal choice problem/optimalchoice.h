372ef2e8b33ce5cad70bc7d2d38e1a2bd2f9d91a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-04 12:35:07 +0300

    Была неверно понята задача вопроса, как следствие, убрано лишнее поле и изменено описание в классе optimalchoice.
    И следующие за этим правки.

--- Optimal choice problem/optimalchoice.h
+++ Optimal choice problem/optimalchoice.h
@@ -11,21 +11,17 @@
 /// \brief The OptimalChoice class
 /// основной класс для нахождения оптимального решения
 /// поставленной задачи с ограничительными условиями, в нашем
-/// случае: существует предел условного веса обьектов и предел
-/// суммарной ценности выбранных обьектов
+/// случае: существует предел условного веса обьектов
 class OptimalChoice : public QObject
 {
     Q_OBJECT
 public:
     explicit OptimalChoice(QObject *parent = nullptr);
     OptimalChoice(int limitOfWeight,
-                  int totalValue,
                   QObject *parent = nullptr);
 
     int getLimitOfWeight() const;
     void setLimitOfWeight(int newLimitOfWeight);
-    int getLimitOfValue() const;
-    void setLimitOfValue(int newLimitOfValue);
 
     ///
     /// \brief selection
@@ -36,11 +32,10 @@ public:
 
 private:
     int limitOfWeight; // предел веса для обьектов
-    int limitOfValue; // предел ценности обьектов
+    int optimalObjectsValue; // общая ценность оптимального набора
     QSet<SomeObject *> selectedCurrentObjects; // текущее состояние собираемого набора
     QSet<SomeObject *> selectedOptimalObjects; // оптимальный набор среди
     // иследованных на данный момент обьектов
-    int optimalObjectsValue; // общая ценность оптимального набора
 
     ///
     /// \brief tryJoin
