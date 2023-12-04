f1aeec4713f861dad09e8b98efbaaa04521e37cb
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-12-04 11:14:49 +0300

    Ради облегчения задачи 2 поля вынесены в параметры метода tryJoin
    Поля:  currentObjectsWeight и allObjectsValue.

--- Optimal choice problem/optimalchoice.h
+++ Optimal choice problem/optimalchoice.h
@@ -40,15 +40,16 @@ private:
     QSet<SomeObject *> selectedCurrentObjects; // текущее состояние собираемого набора
     QSet<SomeObject *> selectedOptimalObjects; // оптимальный набор среди
     // иследованных на данный момент обьектов
-    int currentObjectsWeight; // полный вес набора selectedCurrentObjects
-    int allObjectsValue; // общая ценность всех обьектов, переданных в параметре
     int optimalObjectsValue; // общая ценность оптимального набора
 
     ///
     /// \brief tryJoin
     /// попытка присоединить условный обьект из
     /// списка к текущему набору
-    void tryJoin(int i, QList<SomeObject *> objects);
+    void tryJoin(int i,
+                 int currentObjW,
+                 int potentialObjV,
+                 QList<SomeObject *> objects);
 };
 
 #endif // OPTIMALCHOICE_H
